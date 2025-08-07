27c2d747e8dfb747fc2616e151abad41d3b20e84
ProgrammingRalph <resoluteisresolute@gmail.com>
2025-08-07 01:49:15 -0400

    Add vite cofig update

--- price_finder/vite.config.js
+++ price_finder/vite.config.js
@@ -7,4 +7,10 @@ export default defineConfig({
   plugins: [react(),
     tailwindcss(),
   ],
+  build: {
+    outDir: 'dist',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+  },
 })
